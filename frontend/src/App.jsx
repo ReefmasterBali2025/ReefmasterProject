c0e264f4cb0ec11fa0c12dcc1070eaf95c9e683a
Ghailka Pandunata <pghailka@gmail.com>
2025-01-20 20:07:03 +0800

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ import General from './pages/GeneralCollectionOnly'
 
 const App = () => {
   return (
-    <div >
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
 
       <Navbar />
       <SearchBar />
