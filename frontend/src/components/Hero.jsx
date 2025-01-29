b77c0853d031e4d26f73d0fe64f1a0a7bc86bb7a
Ghailka Pandunata <pghailka@gmail.com>
2025-01-29 11:13:47 +0800

    Update

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { Link } from 'react-router-dom'
 
-
 const Hero = () => {
   return (
     <div className="section py-20">
@@ -12,18 +11,18 @@ const Hero = () => {
           <div className="w-1/3 md:w-full px-4 mb-6">
             <div className="shop bg-white shadow rounded-lg overflow-hidden">
               <div className="shop-img">
-                <video
-                  className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
-                  src={assets.videowysiwyg}
-                  autoPlay
-                  muted
-                  loop
+                  <video
+                    className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
+                    src={assets.videowysiwyg}
+                    autoPlay
+                    muted
+                    loop
                 />
               </div>
               <div className="shop-body p-4 text-center">
                 <h3 className="text-lg font-bold mb-2">WYSIWYG<br />Collection</h3>
-                <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/wysiwygonly'>
-                  Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
+                  <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/WYSIWYG_Only'>
+                    Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
                 </Link>
               </div>
             </div>
@@ -33,18 +32,18 @@ const Hero = () => {
           <div className="w-1/3 md:w-full px-4 mb-6">
             <div className="shop bg-white shadow rounded-lg overflow-hidden">
               <div className="shop-img">
-                <video
-                  className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
-                  src={assets.videogeneral}
-                  autoPlay
-                  muted
-                  loop
+                  <video
+                    className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
+                    src={assets.videogeneral}
+                    autoPlay
+                    muted
+                    loop
                 />
               </div>
               <div className="shop-body p-4 text-center">
                 <h3 className="text-lg font-bold mb-2">Stock List<br />Collection</h3>
                 <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/Stocklist_Only'>
-                  Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
+                    Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
                 </Link>
               </div>
             </div>
@@ -54,20 +53,16 @@ const Hero = () => {
           <div className="w-1/3 md:w-full px-4 mb-6">
             <div className="shop bg-white shadow rounded-lg overflow-hidden">
               <div className="shop-img">
-                <video
-                  className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
-                  src={assets.videofish}
-                  autoPlay
-                  muted
-                  loop
+                  <video
+                    className="w-full h-48 object-cover hover:scale-110 transition ease-in-out duration-300"
+                    src={assets.videofish}
+                    autoPlay
+                    muted
+                    loop
                 />
               </div>
               <div className="shop-body p-4 text-center">
-                <h3 className="text-lg font-bold mb-2">Fish<br />Collection</h3>
-<<<<<<< HEAD
-                <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/Fish&Invert'>
-                  Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
-=======
+                  <h3 className="text-lg font-bold mb-2">Fish<br />Collection</h3>
                 <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/Fish_Only'>
                 Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon'/>
                 </Link>
@@ -90,8 +85,7 @@ const Hero = () => {
               <div className="shop-body p-4 text-center">
                 <h3 className="text-lg font-bold mb-2">Invertebrate<br />Collection</h3>
                 <Link className='cta-btn text-blue-500 hover:text-blue-700 font-semibold inline-flex items-center' to='/Invert_Only'>
-                Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon'/>
->>>>>>> project2
+                    Shop now <img src={assets.go_shop} className='w-5 min-w-5 ml-3' alt='cart-icon' />
                 </Link>
               </div>
             </div>
@@ -99,7 +93,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
-  )
+    )
 }
 
 export default Hero
